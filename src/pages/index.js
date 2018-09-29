34a642bb3c1cd96e0eac9e456af2e486859a9014
sathishsatz123 <sathishrack29@gmail.com>
2018-09-30 09:21:58 +1300

    one

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hi sathish satz</h1>
+    <p>KITE STUDENT.</p>
+    <p>THIS IS MY FIRST ATTEMPT.</p>
+    
+  </Layout>
+)
+
+export default IndexPage
